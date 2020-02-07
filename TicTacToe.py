1e038db85a8286a18d5bbec55bce05a55a5e0121
joshidhawal <joshidhawal2046@gmail.com>
2020-02-07 13:52:14 +0530

    Game Finished But there are bugs

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,7 +6,7 @@ def showBoard(board):
     # this will print the current status of the board,
     # this is not needed in plain way but it's just,
     # here to make this look fancy.
-    print("The present status of the board is :")
+    # print("The present status of the board is :")
     for i in range(3):
         print("----------\n|",end="")
         for j in range(3):
@@ -14,31 +14,50 @@ def showBoard(board):
         print("")
     print("----------")
 
-def playOnBoard(player):
+def playOnBoard(boardIndexHelp,player):
     # This will run the loop letting the assigned player,
     # play until the winning condition is met.
-    showBoard(board_help)
-    played=False
-    pos=int(input("Enter the position where you'd like to enter the symbol : "))
-    #add method to check for a valid position or not.
-    try:
-        a=dict(pos)
-        if(board[a[0]][a[1]]!=player1 or board[a[0]][a[1]]!=player2):
-            board[a[0]][a[1]]=player
-            played = True
-        else :
-            # Gives invalid position prompt
-            # when there's already another player's
-            # input on the position
-            print("That position is already occupied,
-            Enter a valid position")
-    except:
-        print("Enter a valid position")
-        line=lineCheck()
-    showBoard(board)
+    notPlayed = True
+    while (notPlayed):
+        showBoard(board_help)
+        pos=int(input("Enter the position where you'd like to enter the symbol : "))
+        #add method to check for a valid position or not.
+        a=boardIndexHelp(pos)
+        print(a)
+        try:
+            if(board[a[0]][a[1]]!=player1 or board[a[0]][a[1]]!=player2):
+                board[a[0]][a[1]]=player
+                notPlayed = False
+                showBoard(board)
+            else :
+                # Gives invalid position prompt
+                # when there's already another player's
+                # input on the position
+                print("That position is already occupied,Enter a valid position")
+        except Exception as e :
+            print(e)
+            print("Enter a valid position")
+        return notPlayed
 
-def lineCheck():
+    
+def lineCheck(line):
     ## This will check if the winning conditions are true.
+    if (board[0][0]==board[0][1] and board[0][1]==board[0][2] and board[0][2]!=' '):
+        return False
+    if (board[1][0]==board[1][1] and board[1][1]==board[1][2] and board[1][2]!=' '):
+        return False
+    if (board[2][0]==board[2][1] and board[2][1]==board[2][2] and board[2][2]!=' '):
+        return False
+    if (board[0][0]==board[1][0] and board[1][0]==board[2][0] and board[2][0]!=' '):
+        return False
+    if (board[0][1]==board[1][1] and board[1][1]==board[2][1] and board[2][1]!=' '):
+        return False
+    if (board[0][2]==board[1][2] and board[1][2]==board[2][2] and board[2][2]!=' '):
+        return False
+    if (board[0][0]==board[1][1] and board[1][1]==board[2][2] and board[2][2]!=' '):
+        return False
+    if (board[2][0]==board[1][1] and board[1][1]==board[0][2] and board[0][2]!=' '):
+        return False
     
         
 
@@ -51,10 +70,20 @@ player2='O'
 #player2=input("Enter the Symbol you want to use for player2 : ")
 
 print("Player 1 will use : "+player1+" and Player 2 will use : "+player2)
-dict={1:[0,0],2:[0,1],3:[0,2],
+boardIndexHelp={1:[0,0],2:[0,1],3:[0,2],
       4:[1,0],5:[1,1],6:[1,2],
       7:[2,0],8:[2,1],9:[2,2]}
+print("The Game Board is initialised")
 showBoard(board)
-line=False
+line = True
+previousPlayer=player2
+while(line):
+    if previousPlayer==player2:
+        playOnBoard(boardIndexHelp,player1)
+        line = lineCheck(line)
+    else:
+        playOnBoard(boardIndexHelp,player2)
+        line = lineCheck(line)
+
 
 
